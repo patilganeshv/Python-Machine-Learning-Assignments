d4909b1cbf631d030867d8b0ccebdf15ebf39cb9
Ganesh Patil <ganesh.patil@ibm.com>
2026-01-24 00:13:41 +0530

    deleted untracked files

--- 09/Q1_display_jayganesh.py
+++
@@ -1,9 +0,0 @@
-# Write a program which contains one function named as Display() that print "Jay Ganesh" on console
-def Display():
-    print("Jay Ganesh..")
-
-def main():
-    Display()
-
-if __name__ == "__main__":
-    main()
